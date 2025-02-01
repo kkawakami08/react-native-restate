6e8d53ae91096ac63de19f158e52071f02c1d92c
Kaori Kawakami <k.kawakami08@gmail.com>
2025-02-01 12:55:11 -0800

    added Profile layout

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl my-10 font-rubik">
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5 ">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Kaori
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+      <Search />
+    </SafeAreaView>
   );
 }
