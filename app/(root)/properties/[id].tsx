5727a192374451fc9737d78579b374c99291d91a
Kaori Kawakami <k.kawakami08@gmail.com>
2025-02-01 16:57:40 -0800

    explore page implementation

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Property = () => {
   const { id } = useLocalSearchParams();
 
   return (
-    <View>
+    <SafeAreaView>
       <Text>Property {id}</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
